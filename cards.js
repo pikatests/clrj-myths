33782f33fc556feeb68327a59d61358642b76240
pikatests <136384225+pikatests@users.noreply.github.com>
2024-06-14 12:23:43 -0400

    fix asset path and video

--- cards.js
+++ cards.js
@@ -4,7 +4,7 @@ const cards = [
       {
         category: 'MYTH',
         content: 'Abortion is not legal in California.',
-        src: '/assets/cards/card-one-myth.png',
+        src: './assets/cards/card-one-myth.png',
       },
       {
         category: 'FACT',
@@ -17,7 +17,7 @@ const cards = [
         category: 'TESTIMONY',
         content:
           'Abortion is legal and protected in California. Visit https://abortion.ca.gov for a safe space to find abortion information. This website does not save or track your information.',
-        src: '/assets/cards/card-one-fact.png',
+        src: './assets/cards/card-one-fact.png',
       },
     ],
   },
@@ -26,7 +26,7 @@ const cards = [
       {
         category: 'MYTH',
         content: 'Getting abortion pills online is not safe or legitimate.',
-        src: '/assets/cards/card-two-myth.png',
+        src: './assets/cards/card-two-myth.png',
       },
       {
         category: 'FACT',
@@ -34,13 +34,13 @@ const cards = [
           'The idea of ordering abortion pills online can definitely sound scary, thankfully there has been research that shows getting abortion pills from a trusted virtual clinic is safe and legitimate.',
         content:
           'Our friends at the University of California, San Francisco (UCSF) conducted the CHAT research study [https://chatstudy.org], which evaluated experiences of abortion care patients from 3 trusted virtual clinics, Abortion on Demand, Choix, and Hey Jane. These virtual clinics use telehealth so that patients seeking abortion care have the option to speak with a licensed health professional through phone, video call or text message through a virtual platform.',
-        src: '/assets/cards/card-two-fact.png',
+        src: './assets/cards/card-two-fact.png',
       },
       {
         category: 'TESTIMONY',
         content:
           '“At first I could not believe a service like this existed online, but I decided to give it a shot. I was really impressed with the whole process, being able to speak to someone and the packaging of the medication, the whole presentation made me feel better at ease, as it looked professional and legitimate. I had previously tried to order from another foreign online place but in the end did not take the medication since I could not trust it.”',
-        src: '/assets/cards/card-two-fact.png',
+        src: './assets/cards/card-two-fact.png',
       },
     ],
   },
@@ -50,18 +50,18 @@ const cards = [
         category: 'MYTH',
         content:
           'You can only get reproductive health care in person at a clinic.',
-        src: '/assets/cards/card-three-myth.png',
+        src: './assets/cards/card-three-myth.png',
       },
       {
         category: 'FACT',
         header: `Telemedicine and telehealth are additional ways of getting reproductive health care from a licensed health professional, it's like a regular doctor's appointment but you do not need to go in person.`,
         content: `Telemedicine allows you to talk to a licensed health professional by phone or video call and with telehealth you have the extra option of secure messaging with a provider through an app on your smart device.`,
-        src: '/assets/cards/card-three-fact.png',
+        src: './assets/cards/card-three-fact.png',
       },
       {
         category: 'TESTIMONY',
         content: `“I am very grateful for the accessibility to a provider. Possibly the most emotionally supportive resource I've had regardless of not being in person. It made me feel like I could voice out concerns to a professional if I needed to regardless of having an appointment/time slot. Accessibility can make a process that is already hard for some already so much easier.”`,
-        src: '/assets/cards/card-three-fact.png',
+        src: './assets/cards/card-three-fact.png',
       },
     ],
   },
@@ -70,7 +70,7 @@ const cards = [
       {
         category: 'MYTH',
         content: `You can't trust the abortion pills will work if you take them by yourself at home.`,
-        src: '/assets/cards/card-four-myth.png',
+        src: './assets/cards/card-four-myth.png',
       },
       {
         category: 'FACT',
@@ -79,12 +79,12 @@ const cards = [
 
         Both of these options offer after hours support while you are managing your abortion at home, but if you need another resource you can reach out to The Miscarriage + Abortion Hotline, they provide medical advice from volunteer doctors through text or by phone. The number is 
         (833) 246-2632. The line is available in English and Spanish from 8AM to 11PM in all continental US time zones, and they respond within an hour. `,
-        src: '/assets/cards/card-four-fact.png',
+        src: './assets/cards/card-four-fact.png',
       },
       {
         category: 'TESTIMONY',
         content: `“I appreciate this experience in having the autonomy to be able to manage my own care with adequate support from a nurse practitioner. I felt empowered but I did not feel alone and I felt safe which was the most important to me.”`,
-        src: '/assets/cards/card-four-fact.png',
+        src: './assets/cards/card-four-fact.png',
       },
     ],
   },
@@ -93,7 +93,7 @@ const cards = [
       {
         category: 'MYTH',
         content: 'Telemedicine and telehealth can help everybody.',
-        src: '/assets/cards/card-five-myth.png',
+        src: './assets/cards/card-five-myth.png',
       },
       {
         category: 'FACT',
@@ -103,12 +103,12 @@ const cards = [
 
         Whether it's in person at a clinic or through telehealth, ensuring our communities have access to medically accurate health information and access to the abortion care they feel most comfortable with, free of shame, stigma and without barriers, is essential to the fight for Reproductive Justice!
         `,
-        src: '/assets/cards/card-five-fact.png',
+        src: './assets/cards/card-five-fact.png',
       },
       {
         category: 'TESTIMONY',
         content: `“Felt no judgment about my choice that was best for my family and my career at this point in my life. It was convenient! I just wish you guys [the virtual clinic] accepted medi-cal.”`,
-        src: '/assets/cards/card-five-fact.png',
+        src: './assets/cards/card-five-fact.png',
       },
     ],
   },
